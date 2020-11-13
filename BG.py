83f110056104c7e36229c6cf78b54cdd2e457af0
PabloMunozP <pablo.munoz@2mail.udp.cl>
2020-11-13 13:15:57 -0300

    Update BG.py

--- BG.py
+++ BG.py
@@ -60,16 +60,13 @@ def key_generation(p,q):
 
 def encrypt(msg,X0,key):
     X = []
-    X.append(X0)
-    #print('mensaje a cifrar: ',msg)    
+    X.append(X0)    
     hash_bytes=''
     for char in msg:
         bin_text=str(bin(ord(char))).replace('0b','')
         while len(bin_text)<7:
             bin_text= '0'+bin_text
-        #print(char,len(bin_text))
         hash_bytes+=bin_text
-    #print(hash_bytes)
     b = ""
     L = len(str(hash_bytes))
     for i in range(L):
@@ -79,26 +76,21 @@ def encrypt(msg,X0,key):
         b = b_i + b
         new_x = (X[i] ** 2) % key
         X.append(new_x)
-
-    #print("message =", str(msg))
-    #print("b =", b)
     str_m = str(hash_bytes)
     ciphertext = XOR(str_m, b)
-    #print("Ciphertext =", bin_toAscii(ciphertext))
     XL = X[-1]
     X0 = X[0]
     XL_check = pow(X0,pow(2,L),key)
     assert (XL == XL_check)
-    
-    #this tuple represents what is being sent to Alice
     sent_message = (ciphertext, XL)
-    #y = sent_message[1]
     return sent_message
 
 
 #######################################################
 ###########            Decryption           ###########
 #######################################################
+
+
 def decrypt(p,q,encrypted):
 
     ciphertext,XL=encrypted
@@ -108,16 +100,12 @@ def decrypt(p,q,encrypted):
     firstExponent = (((p+1)//4)**L) % (p-1)
     firstPhrase = "({}^{}) mod {}".format(XL,firstExponent,p)
     r_p = pow(XL,firstExponent,p)
-
     secondExponent = (((q+1)//4)**L) % (q-1)
     secondPhrase = "({}^{}) mod {}".format(XL,secondExponent,q)
     r_q = pow(XL,secondExponent,q)
-
     NEWX0 = (q*modInverse(q,p)*r_p + p*modInverse(p,q)*r_q)%N
     NEWX = []
     NEWX.append(NEWX0)
-
-
     b = ""
     for i in range(L):
         string_x = bin(NEWX[-1])[2:]
@@ -126,17 +114,12 @@ def decrypt(p,q,encrypted):
         b = b_i + b
         new_x = (NEWX[i] ** 2) % N
         NEWX.append(new_x)
-
     plaintext = XOR(ciphertext,b)
-    #print("Plaintext  =", bin_toAscii(plaintext))
     return plaintext
-    #checking decrypted ciphertext is the same as the original plaintext
-    #assert(str(m) == str(plaintext))
 
 
 def bin_toAscii(msg):
     msg_split=[msg[i:i+7] for i in range(0,len(msg),7)]
-    #print(msg_split)
     salida=''
     for msg in msg_split:
         while len(msg) < 7:
